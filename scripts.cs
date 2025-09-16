0ad98ade8f2575d9afb3e1eab63d1184bdb32557
Sharkhunterdeadly1 <foreverone1234512@gmail.com>
2025-09-16 20:27:19 +0700

    Add files via upload

---
+++ scripts.cs
@@ -0,0 +1,13 @@
+const budgetInput = document.getElementById('budget');
+const budgetOutput = document.getElementById('budget-output');
+
+
+if (budgetInput && budgetOutput) {
+    
+    budgetOutput.textContent = `Rp ${budgetInput.value} Juta`;
+
+   
+    budgetInput.addEventListener('input', () => {
+        budgetOutput.textContent = `Rp ${budgetInput.value} Juta`;
+    });
+}
